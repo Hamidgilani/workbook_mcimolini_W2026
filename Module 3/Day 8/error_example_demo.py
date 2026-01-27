21669802c0030023ad861b8b6bb95ffb7e62eb97
Michael Cimolini <mcimolini@nait.ca>
2026-01-27 10:59:54 -0700

    IndentationError - Pre-fix

--- Module 3/Day 8/error_example_demo.py
+++ Module 3/Day 8/error_example_demo.py
@@ -4,5 +4,12 @@
 variable = 1
 
 # SyntaxError: expected ':'
-if variable == 1 # as I've left out the : this is a syntax error
+if variable == 1: # as I've left out the : this is a syntax error
     print("Yay!")
+
+# IndentationErrors - This is when your indenting doesn't match
+# Your code may run, but bad things happen.
+if variable == 1:
+    print("Yay!")
+    # IndentationError: unexpected indent
+     print("IndentationError") # I have an extra space here, so I get an IndentationError
