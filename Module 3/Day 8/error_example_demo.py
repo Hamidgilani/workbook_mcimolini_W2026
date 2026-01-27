ea9320f0194693a7c87adcf61b8ab88a75ad65ea
Michael Cimolini <mcimolini@nait.ca>
2026-01-27 11:02:59 -0700

    NumberError - Pre Fix

--- Module 3/Day 8/error_example_demo.py
+++ Module 3/Day 8/error_example_demo.py
@@ -12,4 +12,10 @@ if variable == 1: # as I've left out the : this is a syntax error
 if variable == 1:
     print("Yay!")
     # IndentationError: unexpected indent
-     print("IndentationError") # I have an extra space here, so I get an IndentationError
+    print("IndentationError") # I have an extra space here, so I get an IndentationError
+
+# NameError - This occurs when you try to use a variable that doesn't exist. Often a typo
+# Your code will run, but it will likely crash at some point
+number = 4
+# NameError: name 'nummber' is not defined. Did you mean: 'number'?
+print(f"My number is {nummber}")
