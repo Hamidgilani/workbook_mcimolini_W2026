26e8ba22d7090ad8992e06517a1deef551ecd183
Michael Cimolini <mcimolini@nait.ca>
2026-01-27 10:28:38 -0700

    Match case demo complete

---
+++ Module 3/Day 8/grade_match_case.py
@@ -0,0 +1,21 @@
+letter_grade = input("Enter your letter grade (A, B, C, or D): ").upper()
+# or letter_grade = letter_grade.upper()
+# This will change all of our input to upper-case for ease of comparison
+
+# Convert our letter grade to a GPA
+match letter_grade:
+    case "A":
+        gpa = 4.0
+    case "B":
+        gpa = 3.3
+    case "C":
+        gpa = 2.5
+    case "D":
+        gpa = 2.0
+    case "F":
+        gpa = 1.0
+    case _: # default case
+        print("Could not determine numeric grade")
+        gpa = 0.0
+
+print(f"Your GPA is {gpa}")
