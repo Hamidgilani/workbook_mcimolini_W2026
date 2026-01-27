fdfdafa7750545b39038c61b914167cfed0f227d
Michael Cimolini <mcimolini@nait.ca>
2026-01-27 10:57:24 -0700

    SyntaxError pre fix commit.

---
+++ Module 3/Day 8/error_example_demo.py
@@ -0,0 +1,8 @@
+# SyntaxError - Error in the syntax of our code (the way it's written)
+# Generally, missing a : or similar
+# Your program won't run if there are syntax errors
+variable = 1
+
+# SyntaxError: expected ':'
+if variable == 1 # as I've left out the : this is a syntax error
+    print("Yay!")
