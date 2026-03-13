53ad6259996967c85453983c637e559defe6c3e2
hamid <47733223+Hamidgilani@users.noreply.github.com>
2026-03-13 13:04:51 -0600

    my commit

--- Module 7/Day 24/dictionaries_and_loops_start/fruit_census.py
+++ Module 7/Day 24/dictionaries_and_loops_start/fruit_census.py
@@ -11,6 +11,7 @@ fav_fruit_voters = {
     "james": "orange",
     "mary": "apple",
     "melody": "banana",
+    "johan":"pineapple"
 }
 
 voting_results = {
@@ -18,3 +19,26 @@ voting_results = {
     "apple": 0,
     "orange": 0
 }
+
+print("Voters:")
+for voter in fav_fruit_voters.keys():
+    print(f"- {voter.title()}") # .title() capitalize the first word
+
+# Tally up the votes with .values()
+for vote in fav_fruit_voters.values():
+#    match vote: 
+#       case "banana":
+#            voting_results["banana"] += 1
+#        case "apple":
+#            voting_results["apple"] += 1 
+#        case "orange":
+#            voting_results["orange"] += 1  
+    voting_results[vote] = voting_results.get(vote,0) + 1 # as all of our values are keys in our dictionary, we can use them directly instead of using a 
+# print out the votes
+print("Voting Results:")
+#print(voting_results)            
+
+# We can do better using .items()
+for fruit, vote_count in voting_results.items():
+    print(f"- The fruit: {fruit} has {vote_count} votes")
+
